623a65e90c0989866d03c5ab855513736cc64616
thomann061 <thomann061@gmail.com>
2015-12-01 17:11:07 -0700

    Create WordNet.java

---
+++ WordNet.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package a06;
+
+/**
+ *
+ * @author Jarom_2
+ */
+public class WordNet {
+
+    public WordNet(String synsets, String hypernyms)
+    {
+    
+    }
+    
+    public Iterable<String> nouns()
+    {
+    
+    }
+    
+    public boolean isNoun(String word)
+    {
+        
+    }
+    
+    public int distance(String nounA, String nounB)
+    {
+    
+    }
+    
+    public String sap(String nounA, String nounB)
+    {
+        
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
