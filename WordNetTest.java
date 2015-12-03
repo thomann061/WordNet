ac155d7c24c289a609aac75133307a1e54122e67
thomann061 <thomann061@gmail.com>
2015-12-02 18:46:16 -0700

    Create WordNetTest.java

---
+++ WordNetTest.java
@@ -0,0 +1,42 @@
+/**
+ * Program: WordNetTest.java
+ * Project: Wordnet
+ * Package: a06
+ * Author:  Jacob Thomann
+ * Created: Dec 2, 2015
+ */
+
+package a06;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WordNetTest {
+
+	@Test
+	public void testWordNet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNouns() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsNoun() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDistance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSap() {
+		fail("Not yet implemented");
+	}
+
+}
