d69bce38e48bc68a7bffbfd222c208c7ef8c7996
thomann061 <thomann061@gmail.com>
2015-12-02 18:45:43 -0700

    Update WordNet.java

--- WordNet.java
+++ WordNet.java
@@ -1,46 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package a06;
 
+import edu.princeton.cs.algs4.Stack;
+
 /**
- *
- * @author Jarom_2
+ * WordNet - used in junction with @SAP
+ * @author Cameron Vernon
+ * @author Jacob Thomann
  */
 public class WordNet {
-
-    public WordNet(String synsets, String hypernyms)
-    {
+	
+	/**
+	 * constructor takes the name of the two input files
+	 * @param synsets
+	 * @param hypernyms
+	 */
+    public WordNet(String synsets, String hypernyms)  {
     
-    }
-    
-    public Iterable<String> nouns()
-    {
-    
-    }
-    
-    public boolean isNoun(String word)
-    {
-        
-    }
-    
-    public int distance(String nounA, String nounB)
-    {
-    
-    }
-    
-    public String sap(String nounA, String nounB)
-    {
-        
     }
     
     /**
-     * @param args the command line arguments
+     * returns all WordNet nouns
+     * @return
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public Iterable<String> nouns()  {
+	    Stack<String> it = null;
+	    return it;
     }
     
+    /**
+     * is the word a WordNet noun?
+     * @param word
+     * @return
+     */
+    public boolean isNoun(String word)  {
+        return false;
+    }
+    
+    /**
+     * distance between nounA and nounB
+     * @param nounA
+     * @param nounB
+     * @return
+     */
+    public int distance(String nounA, String nounB)  {
+    	return 0;
+    }
+    
+    /**
+     * a synset that is the common ancestor of nounA and nounB
+     * @param nounA
+     * @param nounB
+     * @return
+     */
+    public String sap(String nounA, String nounB)  {
+        return "";
+    }
+    
+    //unit testing
+    public static void main(String[] args) {
+        
+    }
 }
