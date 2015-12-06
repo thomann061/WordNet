7e8cf541208e1871eb7d7d9383a863708ee82638
thomann061 <thomann061@gmail.com>
2015-12-05 22:27:18 -0700

    Update WordNetTest.java

--- WordNetTest.java
+++ WordNetTest.java
@@ -9,34 +9,43 @@
 package a06;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class WordNetTest {
-
-	@Test
-	public void testWordNet() {
-		fail("Not yet implemented");
-	}
+	
+	private WordNet wordnet = new WordNet("src/synsets.txt", "src/hypernyms.txt");
 
 	@Test
 	public void testNouns() {
-		fail("Not yet implemented");
+		int count = 0;
+		for(String el : wordnet.nouns()) {
+			count++;
+		}
+		assertEquals(119188, count);
 	}
 
 	@Test
 	public void testIsNoun() {
-		fail("Not yet implemented");
+		assertTrue(wordnet.isNoun("punk"));
+		assertTrue(wordnet.isNoun("worm"));
+		assertTrue(wordnet.isNoun("white_marlin"));
+		assertFalse(wordnet.isNoun("lo"));
+		assertFalse(wordnet.isNoun("cumbersome"));
+		assertFalse(wordnet.isNoun("loll"));
 	}
 
 	@Test
 	public void testDistance() {
-		fail("Not yet implemented");
+		assertEquals(5, wordnet.distance("worm", "bird"));
+		assertEquals(23, wordnet.distance("white_marlin", "mileage"));
+		assertEquals(33, wordnet.distance("Black_Plague", "black_marlin"));
+		assertEquals(27, wordnet.distance("American_water_spaniel", "histology"));
+		assertEquals(29, wordnet.distance("Brown_Swiss", "barrel_roll"));
 	}
 
 	@Test
 	public void testSap() {
-		fail("Not yet implemented");
+		assertEquals("physical_entity", wordnet.sap("individual", "edible_fruit"));
 	}
 
 }
